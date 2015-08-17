b87058b0e84f8a3ea42d69ec2e0425c52eb4879a
dxeware <dxeware@gmail.com>
2015-08-16 21:46:09 -0700

    Checking if input present and adding errorMsg if empty

--- js/inst_searcher.js
+++ js/inst_searcher.js
@@ -1,1 +1,28 @@
-angular.module('instagramSearcher', []);
+var DEBUG_MODE = true;
+var debug = function(msg) {
+  if (DEBUG_MODE === true) {
+      console.log("DEBUG:", msg);
+  }
+};
+
+angular.module('instagramSearcher', [])
+  .controller('inputCtrl', function($scope) {
+        $scope.input = {
+          searchText: ''
+        };
+
+        // Validity checking of answers and show story is OK
+        $scope.submit = function() {
+            console.log("Caught form submission!");
+
+            console.log("Form valid = " + $scope.myForm.$valid);
+            if( !$scope.myForm.$valid ) {
+              $scope.errorMsg = "All inputs are required!";
+              debug($scope.errorMsg);
+            } else {
+                debug('All inputs are OK');
+                $scope.errorMsg = '';
+            }
+
+        };
+});
