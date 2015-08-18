1afa76eadfb326129ac1b5509aac44bccb35761e
dxeware <dxeware@gmail.com>
2015-08-18 07:31:42 -0700

    First attempt at http Instagram API working....needs some more updates

--- js/inst_searcher.js
+++ js/inst_searcher.js
@@ -5,13 +5,27 @@ var debug = function(msg) {
   }
 };
 
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
 angular.module('instagramSearcher', ['ngAnimate'])
-  .controller('inputCtrl', function($scope) {
+  .controller('inputCtrl', function($scope, $http) {
         $scope.input = {
           inputText: '',
           searchText: ''
         };
         $scope.searching = false;
+        $scope.searchSuccess = false;
+        $scope.numResults = 0;
+        $scope.href_url = '';
+        $scope.image_url = '';
+        $scope.result = {};
 
         // Validity checking of answers and show story is OK
         $scope.submit = function() {
@@ -27,7 +41,40 @@ angular.module('instagramSearcher', ['ngAnimate'])
                 $scope.input.searchText = $scope.input.inputText;
                 $scope.input.inputText = '';
                 $scope.searching = true;
+                sleep(100000000000);
+                $scope.searchTag($scope.input.searchText);
             }
 
+        };
+
+        $scope.searchTag = function(text) {
+          var url = "https://api.instagram.com/v1/tags/" + text + "/media/recent";
+          debug("url = " + url);
+          var request = {
+            callback: 'JSON_CALLBACK',
+            client_id: '6b80d06561f948c59dfff1b843ab12f6'
+          };
+
+          $http({
+            method: 'JSONP',
+            url: url,
+            params: request
+          })
+          .success(function(result) {
+            debug("Instagram API Success");
+            $scope.displayImages(result);
+
+          })
+          .error(function() {
+            debug('Instagram API errorMsg');
+          });
+        };
+
+        $scope.displayImages = function(result) {
+          $scope.numResults = result.data.length;
+          $scope.searchSuccess = true;
+          $scope.result = result;
+          $scope.href_url = result.data[0].link;
+          $scope.image_url = result.data[0].images.standard_resolution.url;
         };
 });
